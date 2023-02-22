8a0aa4a96a6973880be984f3ab3032a03b0617f0
thanhdoan1606 <thanh1995bc@gmail.com>
2023-02-23 02:36:32 +0700

    taolayout

---
+++ taolayoutchotrangweb/footer.php
@@ -0,0 +1,13 @@
+<style>
+    header,
+    footer {
+        padding: 1em;
+        color: white;
+        background-color: black;
+        clear: left;
+        text-align: center;
+    }
+</style>
+<footer>
+<p>Copyright & copy;<a href="">W3Schools.com</a></p>
+</footer>
---
+++ taolayoutchotrangweb/header.php
@@ -0,0 +1,16 @@
+<style>
+    div.container {
+        width: 100%;
+        border: 1px solid gray;
+    }
+
+    header,
+    footer {
+        padding: 1em;
+        color: white;
+        background-color: black;
+        clear: left;
+        text-align: center;
+    }
+</style>
+<h1>City Gallery</h1>
